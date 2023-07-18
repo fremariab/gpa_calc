f07ecfcfda4879270e34640cf2c2b57d932e42bd
fremariab <fredamariabeech@gmail.com>
2023-07-18 16:03:21 +0000

    added models and migrated

--- evaluation_criteria/models.py
+++ evaluation_criteria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class EvaluationCriteria(models.Model):
+    name = models.CharField(max_length=50)
+    weighting = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
